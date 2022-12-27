3864981ea8d04dd1c64f6577a3bc150cff8aab21
Nataleesha <frojlan@gmail.com>
2022-12-27 20:59:52 +0200

    2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.getElementById('ingredients');
+
+const items = [];
+
+ingredients.forEach(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+  itemEl.classList.add('item');
+  items.push(itemEl);
+});
+
+list.append(...items);
