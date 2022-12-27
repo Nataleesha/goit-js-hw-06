d20de5ebd90f677a823e8cae3cec915359a9bc9c
Nataleesha <frojlan@gmail.com>
2022-12-27 18:02:30 +0200

    1

---
+++ src/js/task-01.js
@@ -0,0 +1,9 @@
+const categories = document.querySelectorAll('.item');
+
+console.log(categories);
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach(category => {
+  console.log(`Category: ${category.children[0].textContent}`);
+  console.log(`Elements: ${category.children[1].children.length}`);
+});
