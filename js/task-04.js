159b172d2a731d6cddcc162e19956853bcc9a880
Nataleesha <frojlan@gmail.com>
2023-01-04 20:01:55 +0200

    4

---
+++ js/task-04.js
@@ -0,0 +1,16 @@
+let counterValue = 0;
+
+const counterEl = document.getElementById('value');
+const decreaseBtn = document.querySelector('button[data-action=decrement]');
+const increaseBtn = document.querySelector('button[data-action=increment]');
+
+decreaseBtn.addEventListener('click', () => {
+    counterValue -= 1;
+    counterEl.innerHTML = counterValue;
+})
+
+increaseBtn.addEventListener('click', () => {
+    counterValue += 1;
+    counterEl.innerHTML = counterValue;
+})
+
